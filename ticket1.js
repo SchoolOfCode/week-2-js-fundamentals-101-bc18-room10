1ec0745da918f8c084af80f09230585773e2406d
Shanti Rai <shantirai@MacBookAir.lan>
2024-12-02 12:39:01 +0000

    Worked through problem 1 MVP 1

---
+++ ticket1.js
@@ -0,0 +1,17 @@
+console.log("Hello Hello");
+console.log("Hello there");
+
+//Write a JavaScript function convertGBPtoEUR DONE
+//function declaration 
+function convertGBPtoEUR(gBP) {   
+    let euros = gBP * 1.21;
+    return euros; 
+};
+
+//that takes in a specified amount in £, DONE
+//and converts it to € using a given exchange rate. DONE
+//The function should return the converted amount. DONE
+
+
+console.log(convertGBPtoEUR(1));
+console.log(convertGBPtoEUR(200));
