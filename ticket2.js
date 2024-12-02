d8476da1c7c4209dc9f7fde4fb76c8a9da5471db
Jacob Smith <183931179+SmithyJ98@users.noreply.github.com>
2024-12-02 15:36:21 +0000

    Ticket 2 JS added

---
+++ ticket2.js
@@ -0,0 +1,50 @@
+// Develop a JavaScript function displayTemperatureInCelsius DONE
+
+const displayTemperatureInCelsius = (fh)=> {
+    const celFormula = ((fh-32) / (9 / 5)) ;
+    return celFormula;
+}
+
+// that takes in a numerical Fahrenheit temperature reading DONE
+// and converts it to Celsius DONE
+//, returning the number representing the Celsius value. DONE
+
+// Implement another function displayTemperatureInFahrenheit DONE
+
+const displayTemperatureInFahrenheit = (cel)=> {
+    const fahFormula = ((cel * (9 / 5)) + 32) ;
+    return fahFormula;
+}
+
+// That takes in a numerical Celcius temperature reading DONE
+// and converts it to Fahrenheit DONE
+//, returning the number representing the Fahrenheit value. DONE
+
+// Create a function toggleTemperatureDisplay DONE
+
+let lastValue = "c";
+
+const toggleTemperatureDisplay = (degrees)=> {   //degrees = 20
+
+   if (lastValue === "c") {
+    lastValue = "f";
+    return displayTemperatureInFahrenheit(degrees);
+   } else if (lastValue ==="f") {
+    lastValue = "c";
+    return displayTemperatureInCelsius(degrees);
+   }
+}
+
+// that returns either Celsius or Fahrenheit 
+// depending on which was the last returned (if the last value returned was Fahrenheit, then you’ll want to return Celsius and vice versa). 
+// Start with a default of the last returned format being Celsius.
+
+// const toggleTemperatureDisplay = (degrees, unit)=> {   //degrees for amount, unit for unit of measurement
+//     if (unit === "c") {
+//         return displayTemperatureInFahrenheit(degrees);
+//     } else if (unit ==="f") {
+//         return displayTemperatureInCelsius(degrees);
+//     } else if (unit !=("c" || "f")) {
+//         return "Error: Unit should be c or f"; 
+//     }
+// }
